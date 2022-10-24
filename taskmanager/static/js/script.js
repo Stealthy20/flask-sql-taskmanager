8c154d28b65240369ee878c30472523f645fab69
Stealthy20 <kjellin.fredrik@live.se>
2022-10-24 13:12:55 +0000

    a

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     const sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    const datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    const selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });

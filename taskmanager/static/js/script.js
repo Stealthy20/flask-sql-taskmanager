c995cc1f8ad084d2153f93c207fe05b5a5fd60d0
Stealthy20 <kjellin.fredrik@live.se>
2022-10-24 18:59:25 +0000

    project done

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
-    const sidenav = document.querySelectorAll(".sidenav");
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    const datepicker = document.querySelectorAll(".datepicker");
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
     // select initialization
-    const selects = document.querySelectorAll("select");
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
